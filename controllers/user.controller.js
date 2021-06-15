6e5c15262bef8d6a320861f85a376eba036067dd
Sourav <people.outofbox@gmail.com>
2021-06-15 12:25:15 +0530

    final touches

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,16 +8,36 @@ const getusers = (req, res) => {
     .catch(err => res.send(err))
 };
 const addUser = (req, res) => {
-    console.log(req.body)
-    const newUser = new User({...req.body});
-    bryptjs.genSalt(10, (err, salt) => {
-        bryptjs.hash(newUser.password, salt, (err, hash) => {
-            newUser.password = hash;
-            newUser.save()
-            .then(user => {return res.redirect("/login")})
-            .catch(err => res.send(err));
-        })
+    const {name, email, password, cpassword} = req.body;
+    let errors = [];
+    User.find({email: email})
+    .then(users => {
+        console.log(users)
+        if(users.length) {
+            errors['email'] = 'This email already exists';
+
+            res.render('register', {
+                name,
+                email,
+                errors: {...errors} 
+            })
+        } else {
+            console.log(req.body)
+            const newUser = new User({...req.body});
+            bryptjs.genSalt(10, (err, salt) => {
+                bryptjs.hash(newUser.password, salt, (err, hash) => {
+                    newUser.password = hash;
+                    newUser.save()
+                    .then(user => {return res.redirect("/login")})
+                    .catch(err => res.send(err));
+                })
+            })
+        }
     })
+    .catch(error => {
+        console.log(error);
+    })
+    
     
 };
 const login = (req, res) => {
